934cd24bfb07b2dd14671d20cda31b71997e9660
Ben Kim <bhk1019@gmail.com>
2018-03-24 02:06:52 +0000

    completed quiz 2

---
+++ app/controllers/planets_controller.rb
@@ -0,0 +1,21 @@
+class PlanetsController < ApplicationController
+
+  def index
+    @planets = Planet.all
+  end
+  
+  def new
+    @planet = Planet.new
+  end
+
+  def create
+    @planet = Planet.create(planet_params)
+    redirect_to planets_path
+  end
+
+  private
+
+  def planet_params
+    params.require(:planet).permit([:name, :description])
+  end
+end
